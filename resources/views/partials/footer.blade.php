4dd267892cbcc59a5f64a8c40136c701286f8d0b
Rafi Ramadhan <rafiramadhanarr17@gmail.com>
2025-10-28 22:57:16 +0700

    fix anchor

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,7 +1,7 @@
 <div class="container-fluid bg-light pt-5 px-sm-3 px-md-5">
     <div class="row">
         <div class="col-lg-3 col-md-6 mb-5">
-            <a href="index.html" class="navbar-brand">
+            <a href="{{ route('home') }}" class="navbar-brand">
                 <h1 class="mb-2 mt-n2 display-5 text-uppercase"><span class="text-primary">Kisah</span>Jawa</h1>
             </a>
             <p>Portal berita yang menyajikan cerita menarik seputar kriminal, misteri, film & review, opini, dan sejarah. Menghadirkan informasi yang informatif bagi pembaca.</p>
