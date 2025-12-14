57655ed05b4f16982834140888010e6f09648d44
Rafi Ramadhan <rafiramadhanarr17@gmail.com>
2025-12-14 11:56:29 +0700

    add google analytics

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -42,5 +42,15 @@
 
     <!-- Template Javascript -->
     <script src="{{ asset('js/main.js') }}"></script>
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-Z116C021SE"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag() { dataLayer.push(arguments); }
+        gtag('js', new Date());
+
+        gtag('config', 'G-Z116C021SE');
+    </script>
 </body>
 </html>
