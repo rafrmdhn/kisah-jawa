4d2612fc37f8f8668fdd241ddd3fbfd554d3c47e
Rafi Ramadhan <rafiramadhanarr17@gmail.com>
2026-01-10 10:44:34 +0700

    style: name editor change

--- resources/views/news/show.blade.php
+++ resources/views/news/show.blade.php
@@ -1,6 +1,23 @@
 @extends('layouts.main')
 
 @section('container')
+    <style>
+        .article-content figure.image {
+            margin: 12px auto;
+            max-width: 100%;
+        }
+
+        .article-content figure.image.image_resized img {
+            width: 100% !important;
+            height: auto !important;
+        }
+
+        .article-content figure.image:not(.image_resized) img {
+            width: auto !important;     /* hormati width="200" */
+            max-width: 100% !important;
+            height: auto !important;
+        }
+    </style>
     <!-- Breadcrumb Start -->
     <div class="container-fluid">
         <div class="container">
